0500d21612951ff50cebfaa7ae457f8cbe8ad30a
Ivan Mamtsev <justgodshot@gmail.com>
2021-08-20 21:01:10 +0300

    added tests for yaml files

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -1,7 +1,6 @@
 import pytest
 from gendiff.generate_diff import generate_diff
 
-
 RESULT = '''{
   - follow: false
     host: hexlet.io
@@ -14,12 +13,36 @@ RESULT = '''{
 PATH = 'tests/fixtures/'
 
 @pytest.fixture
-def files():
+def jsons():
     file1 = PATH + 'file1.json'
     file2 = PATH + 'file2.json'
     yield (file1, file2)
 
 
-def test_generate_diff(files):
-    first, second = files
+@pytest.fixture
+def yamls():
+    file1 = PATH + 'file1.yaml'
+    file2 = PATH + 'file2.yml'
+    yield (file1, file2)
+
+
+@pytest.fixture
+def wrongs():
+    file1 = PATH + 'file1.jpeg'
+    file2 = PATH + 'file2.txt'
+    yield (file1, file2)
+
+
+def test_diff_jsons(jsons):
+    first, second = jsons
     assert generate_diff(first, second) == RESULT
+
+
+def test_diff_yamls(yamls):
+    first, second = yamls
+    assert generate_diff(first, second) == RESULT
+
+
+#def test_wrong_file_format(wrongs):
+#    first, second = wrongs
+#    assert generate_diff(first, second) == 'wrong'
