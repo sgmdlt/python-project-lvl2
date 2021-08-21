d38e67cda85202b9061c29bec4d5f29167bf51ac
Ivan Mamtsev <justgodshot@gmail.com>
2021-08-22 02:22:50 +0300

    changed diff for nested structures

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,6 +1,4 @@
-import json
-from itertools import chain
-
+from gendiff.formaters.stylish import format_
 from gendiff.parsers import parse_files
 
 
@@ -11,8 +9,12 @@ def get_diff(old, new):
     kept = new.keys() & old.keys()
 
     for k_key in kept:
-        if old[k_key] == new[k_key]:
+        if isinstance(old[k_key], dict) and isinstance(new[k_key], dict):
+            diff[(k_key, 'nested')] = get_diff(old[k_key], new[k_key])
+        
+        elif old[k_key] == new[k_key]:
             diff[(k_key, 'kept')] = old[k_key]
+        
         else:
             diff[(k_key, 'removed')] = old[k_key]
             diff[(k_key, 'added')] = new[k_key]
@@ -23,44 +25,6 @@ def get_diff(old, new):
         diff[(a_key, 'added')] = new[a_key]
 
     return diff  # { (key, state): value }
-
-
-def _sort(diff):
-    order = {
-        'removed': 1,
-        'added': 2,
-        'kept': 3,
-    }
-    state_sorted = sorted(diff, key=lambda pair: order.get(pair[1]))
-    return sorted(state_sorted, key=lambda pair: pair[0])
-
-
-def jsonify(value):
-    if not isinstance(value, str):
-        value = json.JSONEncoder().encode(value)
-    return value
-
-
-def format_(diff, indents=2, order=_sort):
-    result = []
-    view = '{ind}{sign} {key}: {value}'.format
-    signs = {
-        'removed': '-',
-        'added': '+',
-        'kept': ' ',
-    }
-
-    for key, state in order(diff):
-        value = jsonify(diff.get((key, state)))
-        result.append(view(
-            ind=' ' * indents,
-            sign=signs.get(state),
-            key=key,
-            value=value,
-        ))
-    result = chain('{', result, '}')
-
-    return '\n'.join(result)
 
 
 def generate_diff(first_file, second_file):
