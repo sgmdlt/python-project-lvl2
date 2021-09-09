e3766c6506ce3941c9426c5f124d3602d49b464a
Ivan Mamtsev <justgodshot@gmail.com>
2021-09-10 02:52:44 +0300

    refactored output

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,27 +1,22 @@
-#def sort_diff(diff):
-#    order = {
-#        'removed': 1,
-#        'added': 2,
-#        'unchanged': 3,
-#        'changed': 4,
-#        'nested': 5,
-#    }
-#    sorted_diff = {}
-#    sort_states = sorted(diff, key=lambda pair: order.get(pair[1]))
-#    sort_names = sorted(sort_states, key=lambda pair: pair[0])
-#    for key in sort_names:
-#        sorted_diff[key] = diff[key]
-#    return sorted_diff
+def format_value(value):
+    if isinstance(value, dict):
+        return '[complex value]'
+    translation = {
+        None: 'null',
+        False: 'false',
+        True: 'true',
+    }
+    return translation.get(value, "'{0}'".format(value))
 
 
 def make_line(state, path, old=None, new=None, value=None):
     path = '.'.join(path)
     if state == 'removed':
-        line = 'Property {0} was removed'.format(path)
+        line = "Property '{0}' was removed".format(path)
     elif state == 'changed':
-        line = 'Propetry {0} was updated. From {1} to {2}'.format(path, old, new)  # noqa: E501
+        line = "Property '{0}' was updated. From {1} to {2}".format(path, old, new)  # noqa: E501
     elif state == 'added':
-        line = 'Propetry {0} was added with value: {1}'.format(path, value)
+        line = "Property '{0}' was added with value: {1}".format(path, value)
     else:
         line = ''
     return line
@@ -39,10 +34,11 @@ def plain(diff, path=None):
 
         value = diff.get((key, state))
         if state == 'changed':
-            old_value = value['old_value']
-            new_value = value['new_value']
+            old_value = format_value(value['old_value'])
+            new_value = format_value(value['new_value'])
             line = make_line(state, path, old_value, new_value)
         else:
+            value = format_value(value)
             line = make_line(state, path, value=value)
         path.pop()
         result.append(line)
