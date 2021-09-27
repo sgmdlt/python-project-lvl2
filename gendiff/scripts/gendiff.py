8248ffc70411238b66537ad0f8dc92dc7d58928c
Ivan Mamtsev <justgodshot@gmail.com>
2021-09-28 01:31:35 +0300

    moved out argparses

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,26 +1,11 @@
 #!/usr/bin/env python
-import argparse
-
-from gendiff.formaters.formaters import DEFAULT_STYLE, FORMATERS
+from gendiff.cli import get_cli_args
 from gendiff.generate_diff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(
-        prog='gendiff',
-        description='Generate diff',
-    )
-    parser.add_argument('first_file', help='path to first file')
-    parser.add_argument('second_file', help='path to second file')
-    parser.add_argument(
-        '-f',
-        '--format',
-        help='set format of output (default: {0})'.format(DEFAULT_STYLE),
-        choices=FORMATERS,
-        default=DEFAULT_STYLE,
-    )
-    args = parser.parse_args()
-    diff = generate_diff(args.first_file, args.second_file, args.format)
+    first_path, second_path, formater = get_cli_args()
+    diff = generate_diff(first_path, second_path, formater)
     print(diff)
 
 
