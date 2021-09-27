cf664e63769f34be1c9e05e2fde57df710f0c49b
Ivan Mamtsev <justgodshot@gmail.com>
2021-09-28 01:31:20 +0300

    added file reading

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,9 +1,14 @@
 from gendiff.differ import get_diff
 from gendiff.formaters.formaters import DEFAULT_STYLE, format_output
-from gendiff.parsers import parse_files
+from gendiff.parsers import parse_data
+from gendiff.reader import get_file
 
 
-def generate_diff(first_file, second_file, style=DEFAULT_STYLE):
-    first, second = parse_files(first_file, second_file)
-    diff = get_diff(first, second)
+def generate_diff(first_path, second_path, style=DEFAULT_STYLE):
+    first_data, first_format = get_file(first_path)
+    second_data, second_format = get_file(second_path)
+    diff = get_diff(
+        parse_data(first_data, first_format),
+        parse_data(second_data, second_format),
+    )
     return format_output(diff, style)
