ddef9058eb2159c9105325a8a0047e778446a3e7
Ivan Mamtsev <justgodshot@gmail.com>
2021-09-29 03:25:22 +0300

    added json ouput test and refactored tests with parametrize

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -12,71 +12,44 @@ PLAIN_EXPECTED = '''{
 }'''
 
 
+PLAIN_FILES = {
+    'plain_files_default_output': ('file1.json', 'file2.json', '', None, PLAIN_EXPECTED),
+}
+
+
+NESTED_FILES = {
+    'nested_files_default_output': ('file1.json', 'file2.yml', 'nested', None, 'stylish_diff.txt'),
+    'nested_files_plain_output': ('file1.yaml', 'file2.json', 'nested', 'plain', 'plain_diff.txt'),
+    'nested_files_json_output': ('file1.json', 'file2.yml', 'nested', 'json', 'json_diff.txt'),
+}
+
+
 def get_fixture_path(file_name, directory=''):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(current_dir, 'fixtures', directory, file_name)
 
 
-@pytest.fixture
-def plain_jsons():
-    file1 = get_fixture_path('file1.json')
-    file2 = get_fixture_path('file2.json')
-    yield (file1, file2)
+@pytest.mark.parametrize(
+    "first_file, second_file, dir, style, output", 
+    PLAIN_FILES.values(),
+    ids=PLAIN_FILES.keys(),
+)
+def test_plain_files(first_file, second_file, dir, style, output):
+    file1 = get_fixture_path(first_file, dir)
+    file2 = get_fixture_path(second_file, dir)
+    assert generate_diff(file1, file2, style) == output
 
 
-@pytest.fixture
-def plain_yamls():
-    file1 = get_fixture_path('file1.yaml')
-    file2 = get_fixture_path('file2.yml')
-    yield (file1, file2)
-
-
-@pytest.fixture
-def nested_jsons():
-    file1 = get_fixture_path('file1.json', 'nested')
-    file2 = get_fixture_path('file2.json', 'nested')
-    yield (file1, file2)
-
-
-@pytest.fixture
-def nested_yamls():
-    file1 = get_fixture_path('file1.yaml', 'nested')
-    file2 = get_fixture_path('file2.yml', 'nested')
-    yield (file1, file2)
-
-
-@pytest.fixture
-def stylish_output():
-    yield open(get_fixture_path('stylish_diff.txt', 'nested'), 'r').read()
-
-
-@pytest.fixture
-def plain_output():
-    yield open(get_fixture_path('plain_diff.txt', 'nested'), 'r').read()
-
-
-def test_plain_jsons(plain_jsons):
-    first, second = plain_jsons
-    assert generate_diff(first, second) == PLAIN_EXPECTED
-
-
-def test_plain_yamls(plain_yamls):
-    first, second = plain_yamls
-    assert generate_diff(first, second) == PLAIN_EXPECTED
-
-
-def test_stylish_formater(nested_jsons, nested_yamls, stylish_output):
-    first_json, second_json = nested_jsons
-    first_yaml, second_yaml = nested_yamls
-    assert generate_diff(first_json, second_json) == stylish_output
-    assert generate_diff(first_yaml, second_yaml) == stylish_output
-
-
-def test_plain_formater(nested_jsons, nested_yamls, plain_output):
-    first_json, second_json = nested_jsons
-    first_yaml, second_yaml = nested_yamls
-    assert generate_diff(first_json, second_json, style='plain') == plain_output
-    assert generate_diff(first_yaml, second_yaml, style='plain') == plain_output
+@pytest.mark.parametrize(
+    "first_file, second_file, dir, style, output", 
+    NESTED_FILES.values(), 
+    ids=NESTED_FILES.keys(),
+)
+def test_nested_files(first_file, second_file, dir, style, output):
+    file1 = get_fixture_path(first_file, dir)
+    file2 = get_fixture_path(second_file, dir)
+    output_file = open(get_fixture_path(output, dir), 'r').read()
+    assert generate_diff(file1, file2, style) == output_file
 
 
 def test_wrong_file_format():
