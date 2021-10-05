0d0aaf11ba1fa4a3ee85a65701223108d307fce9
Ivan Mamtsev <justgodshot@gmail.com>
2021-10-05 17:30:57 +0300

    splitted get_file

--- gendiff/io.py
+++ gendiff/io.py
@@ -2,6 +2,10 @@ from gendiff.parsers import PARSERS as FORMATS
 
 
 def get_file(file_path):
+    return open(file_path, 'r')
+
+
+def get_extension(file_path):
     file_name = file_path.split('/')[-1]
     file_format = file_name.split('.')[-1]
     supported = ', '.join(FORMATS)
@@ -12,5 +16,8 @@ def get_file(file_path):
             f=file_format,
             sup=supported,
         ))
-    file = open(file_path, 'r')
-    return (file, file_format)
+    return file_format
+
+
+def data_and_format(source, data=get_file, data_format=get_extension):
+    return data(source), data_format(source)
