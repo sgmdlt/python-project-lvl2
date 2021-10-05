37b15f33c0cc1c804a3baf094f516e1fbc03eedb
Ivan Mamtsev <justgodshot@gmail.com>
2021-10-05 17:31:38 +0300

    added default format and refactored imports

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,12 +1,14 @@
 from gendiff.differ import get_diff
-from gendiff.formaters.formaters import DEFAULT_FORMAT, format_output
-from gendiff.io import get_file
+from gendiff.formaters.formaters import format_output
+from gendiff.io import data_and_format
 from gendiff.parsers import parse_data
+
+DEFAULT_FORMAT = 'stylish'
 
 
 def generate_diff(first_path, second_path, style=DEFAULT_FORMAT):
-    first_data, first_format = get_file(first_path)
-    second_data, second_format = get_file(second_path)
+    first_data, first_format = data_and_format(first_path)
+    second_data, second_format = data_and_format(second_path)
     diff = get_diff(
         parse_data(first_data, first_format),
         parse_data(second_data, second_format),
